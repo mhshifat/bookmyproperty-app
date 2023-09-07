b2bda422760f0ad8251ae47047ccba3faa2ba28c
shifat.dev <adnan@lazertechnologies.com>
2023-09-07 15:41:46 +0600

    prisma error fixed

--- app/actions/getFavoriteListings.ts
+++ app/actions/getFavoriteListings.ts
@@ -1,10 +1,11 @@
 import getCurrentUser from '@/app/actions/getCurrentUser';
+import prismaClient from '../libs/db';
 
 export default async function getFavoriteListings() {
   try {
     const currentUser = await getCurrentUser();
     if (!currentUser) return [];
-    const favorites = await prisma?.listing.findMany({
+    const favorites = await prismaClient?.listing.findMany({
       where: {
         id: {
           in: [...(currentUser?.favoriteIds || [])]
